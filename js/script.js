7a743e18b304df5bb0c6ec146d3bb50fa8d8e29e
fikobayashi <fernando.kobayashi@hotmail.com>
2025-04-09 17:08:45 -0300

    include menu mobile

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// scroll da ancoragem de outros dispositivos
+document.querySelectorAll('.header-menu a[href^="#"]').forEach(function(anchor) {
+	anchor.addEventListener('click', function(e) {
+			e.preventDefault();
+			var id = this.getAttribute('href');
+			var target = document.querySelector(id);
+			if (target) {
+					var targetOffset = target.offsetTop;
+					window.scrollTo({
+							top: targetOffset - 58,
+							behavior: 'smooth'
+					});
+			}
+	});
+});
+
+// scroll da ancoragem mobile
+document.querySelectorAll('.menu-mobile a[href^="#"]').forEach(function(anchor) {
+	anchor.addEventListener('click', function(e) {
+			e.preventDefault();
+			var id = this.getAttribute('href');
+			var target = document.querySelector(id);
+			if (target) {
+					var targetOffset = target.offsetTop;
+					window.scrollTo({
+							top: targetOffset - 58,
+							behavior: 'smooth'
+					});
+			}
+	});
+});
+
+
