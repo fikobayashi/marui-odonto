5d870d00175f0f58a27e929c04476174e73e95eb
fikobayashi <fernando.kobayashi@hotmail.com>
2025-04-10 11:13:40 -0300

    script mobile

--- js/script.js
+++ js/script.js
@@ -30,4 +30,19 @@ document.querySelectorAll('.menu-mobile a[href^="#"]').forEach(function(anchor) 
 	});
 });
 
+// menu mobile equivalente ao :hover no desktop
 
+const toggleButton = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu-mobile');
+const items = document.querySelectorAll('.item-menu');
+
+toggleButton.addEventListener('click', () => {
+  menu.classList.toggle('mostrar');
+});
+
+// Esconde o menu ao clicar em um item
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    menu.classList.remove('mostrar');
+  });
+});
